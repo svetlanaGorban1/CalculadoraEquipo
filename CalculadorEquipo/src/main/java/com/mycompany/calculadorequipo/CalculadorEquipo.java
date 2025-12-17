15138c3f2e4dce8f32692778cb025af484954a11
svetlanaGorban1 <sgorban01@educantabria.es>
2025-12-17 19:28:31 +0100

    Estructura inicial del proyecto

---
+++ CalculadorEquipo/src/main/java/com/mycompany/calculadorequipo/CalculadorEquipo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.calculadorequipo;
+
+/**
+ *
+ * @author damt110
+ */
+public class CalculadorEquipo {
+
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora Básica ===");
+        System.out.println("Versión 1.0");
+        // Las operaciones serán implementadas por el equipo
+    }
+}
