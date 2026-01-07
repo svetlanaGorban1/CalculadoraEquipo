1c7f0b75e48ff5a985d5a2ba95db1838bf365243
svetlanaGorban1 <sgorban01@educantabria.es>
2026-01-07 16:40:35 +0100

    Añadidas funciones de suma y resta4

--- CalculadorEquipo/src/main/java/com/mycompany/calculadorequipo/CalculadorEquipo.java
+++ CalculadorEquipo/src/main/java/com/mycompany/calculadorequipo/CalculadorEquipo.java
@@ -10,6 +10,7 @@ package com.mycompany.calculadorequipo;
 public class CalculadorEquipo {
 
     
+  //2
     public static void main(String[] args) {
         System.out.println("=== Calculadora Básica ===");
         System.out.println("Versión 1.0");
