906d3295b5217afb413e6aeeb919c7f36cd9d298
svetlanaGorban1 <sgorban01@educantabria.es>
2026-01-07 16:50:30 +0100

    Añadidas funciones de suma y resta nuevo

--- CalculadorEquipo/src/main/java/com/mycompany/calculadorequipo/CalculadorEquipo.java
+++ CalculadorEquipo/src/main/java/com/mycompany/calculadorequipo/CalculadorEquipo.java
@@ -9,8 +9,6 @@ package com.mycompany.calculadorequipo;
  */
 public class CalculadorEquipo {
 
-    
-  //2
     public static void main(String[] args) {
         System.out.println("=== Calculadora Básica ===");
         System.out.println("Versión 1.0");
@@ -24,8 +22,7 @@ public class CalculadorEquipo {
     public static double sumar(double num1, double num2) {
         return num1 + num2;
     }
-
-    //1
+   
     // Función para restar dos números
     public static double restar(double num1, double num2) {
         return num1 - num2;
